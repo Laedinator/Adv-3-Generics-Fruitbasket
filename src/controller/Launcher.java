21d03a5816787fc449d194f079fdddd43005f5bb
Laedinator <62262634+Laedinator@users.noreply.github.com>
2023-11-21 12:12:50 +0100

    Add all functions and add a recursive search function

--- src/controller/Launcher.java
+++ src/controller/Launcher.java
@@ -1,7 +1,6 @@
 package controller;
 
-import model.Apple;
-import model.Fruit;
+import model.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +9,27 @@ import java.util.List;
 public class Launcher {
 
     public static void main(String[] args) {
-       // TODO Try and use the FruitBasket class for different kind of fruits and also for a mix of fruits
+        // TODO Try and use the FruitBasket class for different kind of fruits and also for a mix of fruits
+        Fruit apple = new Apple();
+        Fruit banana = new Banana();
+        Fruit pear = new Pear();
+        Fruit pear2 = new Pear();
+        Fruit apple2 = new Apple();
 
+
+        FruitBasket fruitBasket = new FruitBasket<>();
+        fruitBasket.add(apple);
+        fruitBasket.add(banana);
+        fruitBasket.add(pear);
+        fruitBasket.add(pear2);
+
+        System.out.println(fruitBasket.toString());
+        System.out.println("This fruit was removed:");
+        System.out.println(fruitBasket.get());
+        System.out.println();
+        System.out.println(fruitBasket.toString());
+
+        boolean containsFruit = fruitBasket.contains(apple);
+        System.out.println(containsFruit);
     }
 }
---
+++ src/model/FruitBasket.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Marc Ledermann
+ * <m.ledermann@st.hanze.nl>
+ * Purpose of the program:
+ **/
+public class FruitBasket<F extends Fruit> {
+    List<F> contents = new ArrayList<>();
+
+    public FruitBasket() {
+    }
+
+    public void add(F fruit) {
+        contents.add(fruit);
+    }
+
+    public boolean contains(F fruit) {
+//        check if there are any fruits of this type
+        if (contents.isEmpty()) {
+            return false;
+        }
+        System.out.println("Class to check: " + fruit.getClass());
+        boolean containsClass = false;
+        for (F content : contents) {
+            if (fruit.getClass() == content.getClass()) {
+                containsClass = true;
+            }
+        }
+        return containsClass;
+    }
+
+    public <F> Object get() {
+//        returns the first (earliest added fruit from the basket and removes it)
+        if (contents.isEmpty()) {
+            System.out.println("The basket is empty.");
+            return null;
+        }
+        Object objectToRemove = get(0);
+        boolean remove = contents.remove(objectToRemove);
+        if (!remove) {
+            return null;
+        }
+        return objectToRemove;
+    }
+
+    private <F> Object get(int index) {
+        if (index == contents.size()) {
+            return contents.get(index - 1);
+        }
+        Object oldestFruit = get(index + 1);
+        if (index <= contents.indexOf(oldestFruit)) {
+            return contents.get(index);
+        } else {
+            return oldestFruit;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("The basket contains:\n");
+        for (F content : contents) {
+            stringBuilder.append(content.toString());
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
